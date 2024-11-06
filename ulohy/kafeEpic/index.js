6f589858e56cb9dad749bb92b0ecb805d1dd73ee
Rajský Matyáš <149485684+rrrajsky@users.noreply.github.com>
2024-11-06 15:04:07 +0100

    Delete ulohy/kafeEpic/index.js

--- ulohy/kafeEpic/index.js
+++
@@ -1,8 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/3.1.3/socket.io.min.js"></script>
-<script>
-    var socket = io();
-
-    socket.on('coffee_notification', function(data) {
-        alert(data.message);
-    });
-</script>
